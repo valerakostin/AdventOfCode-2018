98175644891ff39ca5abb7905955a05d592f5b64
Valera Kostin <valera.kostin@gmail.com>
2018-12-01 09:20:11 +0100

    Day1: add solution

---
+++ src/day01/AdventOfCodeDay01.kt
@@ -0,0 +1,31 @@
+package day01
+
+import utils.getLinesFromSources
+
+
+fun solve() {
+    val linesFromSources = getLinesFromSources("InputDay01.txt")
+    val numbers = linesFromSources.map { Integer.parseInt(it) }.toList()
+    println("Task 1 ${numbers.sum()}")
+    println("Task 2 ${task2(numbers)}")
+}
+
+fun task2(numbers: List<Int>): Int {
+    val set = mutableSetOf(0)
+    var currentFrequency = 0
+    while (true) {
+        for (number in numbers) {
+            currentFrequency += number
+            if (set.contains(currentFrequency))
+                return currentFrequency
+            else
+                set.add(currentFrequency)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+
+    println("Day 01:")
+    solve()
+}
