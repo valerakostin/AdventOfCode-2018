57b4d78f55c6d8d822bda17fffc915f9df3b805a
Valera Kostin <valera.kostin@gmail.com>
2018-12-02 19:45:40 +0100

    Day2: initial commit

---
+++ src/day02/AdventOfCodeDay02.kt
@@ -0,0 +1,46 @@
+package day02
+
+
+import utils.Utils.getLinesFromResources
+
+
+private fun task1(lines: List<String>) {
+    var sum2 = 0
+    var sum3 = 0
+
+    lines.forEach { line ->
+        val histogram = line.groupingBy { it }.eachCount()
+        if (histogram.values.contains(2))
+            sum2 += 1
+        if (histogram.values.contains(3))
+            sum3 += 1
+    }
+    println("$sum2 * $sum3 = ${sum2 * sum3}")
+}
+
+private fun task2(lines: List<String>) {
+    for (i in 0 until lines[0].length) {
+        val set = mutableSetOf<String>()
+        for (item in lines) {
+            val array = item.toCharArray()
+            array[i] = i.toChar()
+            val str = String(array)
+            if (!set.add(str)) {
+                str.filter { it != i.toChar() }.toCharArray().forEach { print(it) }
+                break
+            }
+        }
+    }
+}
+
+
+private fun solve() {
+    val lines = getLinesFromResources("InputDay02.txt")
+    task1(lines)
+    task2(lines)
+}
+
+fun main(args: Array<String>) {
+    println("Day 02:")
+    solve()
+}
